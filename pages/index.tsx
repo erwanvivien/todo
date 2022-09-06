842a56a253edc67c64f9479a729f8874f0fbaec4
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-09-06 12:06:05 +0200

    fix: checks if user is connected or not

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,17 @@
+import { withIronSessionSsr } from "iron-session/next";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Button from "../components/Button";
 import Lnk from "../components/Lnk";
+import { UserInfo } from "../prisma/user/type";
+import { sessionOptions } from "../prisma/utils";
 import styles from "../styles/Home.module.css";
 
-const NotLoggedIn: React.FC = () => (
-  <>
-    <h1 className={styles.title}>
-      Begin to <span className={styles.primary}>Do</span>
-    </h1>
+type HomeProps = {
+  user: UserInfo | null;
+};
 
-    <p className={styles.description}>
-      Simple. Fast. Easy.
-      <br />
-      Create and manage tasks
-    </p>
-
-    <Lnk className={styles.highlight} href="/me">
-      Create your account and your first todo list
-    </Lnk>
-  </>
-);
-
-const Home: NextPage = () => {
+const Home: NextPage<HomeProps> = ({ user }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -29,10 +19,32 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <NotLoggedIn />
+        <h1 className={styles.title}>
+          Begin to <span className={styles.primary}>Do</span>
+        </h1>
+
+        <p className={styles.description}>
+          Simple. Fast. Easy.
+          <br />
+          Create and manage tasks
+        </p>
+
+        {!user && (
+          <Lnk href="/me">
+            <Button className={styles.highlight}>Log in</Button>
+          </Lnk>
+        )}
       </main>
     </div>
   );
 };
 
+const getServerSideProps = withIronSessionSsr(async ({ req }) => {
+  const user = req.session.user;
+  return {
+    props: { user: user || null },
+  };
+}, sessionOptions);
+
+export { getServerSideProps };
 export default Home;
